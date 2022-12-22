3e61078c68dabb88cdd167392602bf95ce2f96b4
akor11lyt <akor1@naver.com>
2022-12-22 17:56:27 +0900

    commit

--- codeit-django/costaurant/foods/views.py
+++ codeit-django/costaurant/foods/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.http import Http404
+from foods.models import Menu
 
 
 def index(request):
+    context = dict()
     today = datetime.today().date()
-    context = {"date":today}
+    menus = Menu.objects.all()
+    context["date"] = today
+    context["menus"] = menus
     return render(request, 'foods/index.html', context=context)
+
 
 def food_detail(request, food):
     context = dict()
@@ -20,3 +25,4 @@ def food_detail(request, food):
         raise Http404("이런 음식은 없다구요 !")
         # raise : 파이썬에서 에러를 강제로 발생시킬 때 사용하는 문법
     return render(request, 'foods/detail.html', context=context)
+
