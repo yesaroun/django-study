b9a3c94f9ec38a33da702cc06b608029cb0b5599
yesaroun <akor1@naver.com>
2022-12-27 20:13:55 +0900

    url 매핑

--- doit/mysite/pybo/views.py
+++ doit/mysite/pybo/views.py
@@ -7,3 +7,9 @@ def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    """pybo 내용 출력"""
+    question = Question.objects.get(id=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_list.html', context)
