033aa5833d2d3e00c9e18c83f521f6c65c1709b8
yesaroun <akor1@naver.com>
2022-12-28 17:57:26 +0900

    404 에러 처리

--- doit/mysite/pybo/views.py
+++ doit/mysite/pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -10,6 +10,6 @@ def index(request):
 
 def detail(request, question_id):
     """pybo 내용 출력"""
-    question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
-    return render(request, 'pybo/question_list.html', context)
+    return render(request, 'pybo/question_detail.html', context)
