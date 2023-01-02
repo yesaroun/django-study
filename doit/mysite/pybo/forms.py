764cff35f982f84fd0cdc6b49a82e8e6497fb046
yesaroun <akor1@naver.com>
2023-01-02 17:10:53 +0900

    질문 등록

---
+++ doit/mysite/pybo/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from pybo.models import Question
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ['subject', 'content']
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
+        labels = {
+            'subject': '제목',
+            'content': '내용',
+        }
--- doit/mysite/pybo/views.py
+++ doit/mysite/pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm
 
 def index(request):
     """pybo 목록 출력"""
@@ -21,3 +22,17 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get('content'),
                                create_date=timezone.now())
     return redirect('pybo:detail', question_id=question_id)
+
+def question_create(request):
+    """pybo 질문 등록"""
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+        else:
+            form = QuestionForm()
+        context = {'form': form}
+        return render(request, 'pybo/question_form.html', context)
