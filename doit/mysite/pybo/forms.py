fd1675136ee3561c97360f7eff03451fb43fbc21
yesaroun <akor1@naver.com>
2023-01-03 18:30:31 +0900

    pybo 개발

--- doit/mysite/pybo/forms.py
+++ doit/mysite/pybo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
 
 class QuestionForm(forms.ModelForm):
     class Meta:
@@ -7,9 +7,17 @@ class QuestionForm(forms.ModelForm):
         fields = ['subject', 'content']
         widgets = {
             'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.textarea(attrs={'class': 'form-control', 'rows': 10}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
         }
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
         }
