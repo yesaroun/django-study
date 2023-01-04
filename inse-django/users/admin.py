f26430ed159d860763f8cfbce4972d4a9482f714
yesaroun <akor1@naver.com>
2023-01-04 11:27:29 +0900

    pybo 기초

--- inse-django/users/admin.py
+++ inse-django/users/admin.py
@@ -3,6 +3,7 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("nickname", "phone", "is_business", "gender", "profileIntroduce", )     # 이 조건들 보여줌
-    list_filter = ("is_business", )     # 필터 기능
-    search_fields = ("nickname", "phone" )      # 닉네임 기준 검색 기능
+    # list_display = ("nickname", "phone", "is_business", "gender", "profileIntroduce", "sns" )     # 이 조건들 보여줌
+    # list_filter = ("is_business", )     # 필터 기능
+    # search_fields = ("nickname", "phone" )      # 닉네임 기준 검색 기능
+    pass
--- inse-django/users/models.py
+++ inse-django/users/models.py
@@ -1,9 +1,11 @@
-from django.db import models
+# from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
-class User(models.Model):
-    nickname = models.CharField(max_length=30)
-    phone = models.PositiveIntegerField()       # 양의 정수형 숫자
-    is_business = models.BooleanField(default=False)    # boolean은 default값이 있어야 한다.
-    gender = models.CharField(max_length=10)
-    profileIntroduce = models.TextField(max_length=200)
+class User(AbstractUser):
+    # nickname = models.CharField(max_length=30)
+    # phone = models.PositiveIntegerField()       # 양의 정수형 숫자
+    # is_business = models.BooleanField(default=False)    # boolean은 default값이 있어야 한다.
+    # gender = models.CharField(max_length=10)
+    # profileIntroduce = models.TextField(max_length=200)
+    # sns = models.CharField(max_length=10, default="")
+    pass
