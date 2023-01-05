f41b1af6bfca06e0ada842fe84dd2c844a959c6e
yesaroun <akor1@naver.com>
2023-01-05 17:45:04 +0900

    게시글 수정, 삭제 버튼

--- doit/mysite/pybo/views.py
+++ doit/mysite/pybo/views.py
@@ -5,26 +5,30 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 def index(request):
     """pybo 목록 출력"""
     # 입력 인자
-    page = request.GET.get('page', '1')     # 페이지
+    page = request.GET.get('page', '1')  # 페이지
     # 조회
     question_list = Question.objects.order_by('-create_date')
 
     # 페이징 처리
-    paginator = Paginator(question_list, 10)        # 페이지당 10개씩 보여 주기
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여 주기
     page_obj = paginator.get_page(page)
 
     context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
+
 
 def detail(request, question_id):
     """pybo 내용 출력"""
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
 
 @login_required(login_url='common:login')
 def answer_create(request, question_id):
@@ -44,6 +48,7 @@ def answer_create(request, question_id):
         context = {'question': question, 'form': form}
         return render(request, 'pybo/question_detail.html', context)
 
+
 @login_required(login_url='common:login')
 def question_create(request):
     """pybo 질문 등록"""
@@ -59,3 +64,37 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """pybo 질문 수정"""
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, "수정권한이 없습니다.")
+        return redirect('pybo:detail', question_id=question_id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question_id)
+
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """pybo 질문 삭제"""
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question_id)
+    question.delete()
+    return redirect('pybo:index')
