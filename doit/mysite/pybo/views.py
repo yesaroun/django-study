4dcea52ecc4ed6adedee5fccf03860f95de28747
yesaroun <akor1@naver.com>
2023-01-06 15:53:05 +0900

    view 정리

--- doit/mysite/pybo/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-
-from .forms import QuestionForm, AnswerForm, CommentForm
-from .models import Question, Answer, Comment
-
