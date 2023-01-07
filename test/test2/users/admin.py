86f393161f25eeb321ecaa26f304e4e760b7c5a0
yesaroun <akor1@naver.com>
2023-01-07 17:40:19 +0900

    커밋할 메세지 입력

--- test/test2/users/admin.py
+++ test/test2/users/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
+from .models import User
 
+@admin.register(User)   # UserAdmin에 등록할 Model 지정(Decorator)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ["name", "age", "sex", ]
+    fieldsets = (
+        ("Profile Info", {
+            "fields": ("email", "password", "is_business"),
+            "classes": ("wide",),
+            },
+        ),
+        ("Permissions", {
+            "fields":{
+                "is_active",
+                "is_staff"
+            }
+            }
+        )
+    )
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = None
+    fields = ("email", "password", "name")
--- test/test2/users/models.py
+++ test/test2/users/models.py
@@ -1,11 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    name = models.CharField(max_length=20)  # Model을 상속받는다.
-    description = models.TextField()        # 긴 텍스트 문장
-    age = models.PositiveIntegerField()     # 양의 정수형
-    sex = models.CharField(max_length=10)
-    is_business = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
+class User(AbstractUser):
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150)
+    is_business = models.BooleanField()
