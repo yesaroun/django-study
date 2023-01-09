90c8790564fdb9cd68ad5f5aa654d622234da8ae
yesaroun <akor1@naver.com>
2023-01-09 11:22:33 +0900

    django

--- oz-backend-django/reviews/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Review
-
-class FeedReviewsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        # fields = "__all__"
-        fields = ("id", "content", "like")
--- oz-backend-django/reviews/tests.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def show_reivew(request):
-    return HttpResponse("This is Review page")
