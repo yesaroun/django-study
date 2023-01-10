62d37de8557b5fa971232662caa7aecd32bb34ab
yesaroun <akor1@naver.com>
2023-01-10 10:20:31 +0900

    flask 세팅

---
+++ flask-study/webscraper/main.py
@@ -0,0 +1,20 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+if response.status_code != 200:
+    print("can't request website")
+else:
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all("section", class_="jobs")
+    for job_section in jobs:
+        job_posts = job_section.find_all('li')
+        job_posts.pop(-1)
+        for post in job_posts:
+            print(post)
+            print("/"*20)
+
+
