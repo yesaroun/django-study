576b5bc31eafeb482f3308f2eab0f2cf993bddba
yesaroun <akor1@naver.com>
2023-01-15 21:51:33 +0900

    shortner 생성

---
+++ testPj/test_1/shortner/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Users(AbstractUser):
+    full_name = models.CharField(max_length=100, null=True)
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING, null=True)
