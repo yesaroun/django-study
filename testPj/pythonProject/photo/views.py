a7b158d1f540b501f51499e5a5083e630d96d9e1
yesaroun <akor1@naver.com>
2023-01-22 22:42:25 +0900

    detail pages

--- testPj/pythonProject/photo/views.py
+++ testPj/pythonProject/photo/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Photo
+from django.shortcuts import render, get_object_or_404
 
 
 def photo_list(request):
     photos = Photo.objects.all()
     return render(request, 'photo/photo_list.html', {'photos': photos})
+
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'photo/photo_detail.html', {'photo': photo})
