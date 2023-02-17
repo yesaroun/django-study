7e90a52dd23f900e9094374af61b1f22c43eb5ef
yesaroun <akor1@naver.com>
2023-02-17 16:18:34 +0900

    feet: Post 모델 생성

--- codeit-django/costory/posts/models.py
+++ codeit-django/costory/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    # 글의 제목, 내용, 작성일, 마지막 수정일
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
+    dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
+
+    def __str__(self):
+        return self.title
+
