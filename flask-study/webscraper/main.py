f16dbc551f92c6e6537bf32bfc641feb39210532
yesaroun <akor1@naver.com>
2023-02-17 12:49:51 +0900

    feat: costory project setting

--- flask-study/webscraper/main.py
+++ flask-study/webscraper/main.py
@@ -1,20 +1,8 @@
-from extractors.indeed import extract_indeed_job
-from extractors.wwr import extract_wwr_jobs
+from flask import Flask
 
-keyword = input("What do you want to search for?")
-
-
-indeed = extract_indeed_job(keyword)
-wwr = extract_wwr_jobs(keyword)
-jobs = indeed + wwr
-
-file = open(f"{keyword}.csv", "w")
-file.write("Position,Company,Location,URL\n")
-
-for job in jobs:
-    file.write(f"{job['position']},{job['company']},{job['location']},{job['link']}\n")
+app = Flask("JobScrapper")
 
 
-file.close()
 
-
+if __name__ == "__main__":
+    app.run()
