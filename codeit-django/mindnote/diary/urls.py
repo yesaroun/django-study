f482bc981dce9e43b22012d40025649247db399a
yesaroun <akor1@naver.com>
2023-02-17 16:04:34 +0900

    feet: url 세팅

---
+++ codeit-django/mindnote/diary/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    # path('diary/', include(views.page_list)),
+    # path('diary/info', include(views.info)),
+    # path('diary/write', include(views.page_create)),
+    # path('diary/page/<int:page_id>', include(views.page_detail)),
+    # path('diary/page/<int:page_id>/edit', include(views.page_update)),
+    # path('diary/page/<int:page_id>/delete', include(views.page_delete)),
+]
