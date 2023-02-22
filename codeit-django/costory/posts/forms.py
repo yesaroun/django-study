cb88e929fde90a948d795c7d69d8848dea5d0383
yesaroun <akor1@naver.com>
2023-02-22 09:43:32 +0900

    fix: model form 적용

--- codeit-django/costory/posts/forms.py
+++ codeit-django/costory/posts/forms.py
@@ -1,6 +1,9 @@
 from django import forms
+from .models import Post
 
 
-class PostForm(forms.Form):
-    title = forms.CharField(max_length=50, label='제목')
-    content = forms.CharField(label='내용', widget=forms.Textarea)
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
