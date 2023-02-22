2ba364d173ab5f642ca74cf775ae040c8cdb8827
yesaroun <akor1@naver.com>
2023-02-23 08:34:57 +0900

    fix: validater를 직접 생성해서 유효성 검사

--- codeit-django/costory/posts/models.py
+++ codeit-django/costory/posts/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from .validators import validate_symbols
 
 
 class Post(models.Model):
     # 글의 제목, 내용, 작성일, 마지막 수정일
     title = models.CharField(max_length=50, unique=True, error_messages={'unique': '이미 있는 제목이네요!'})
-    content = models.TextField(validators=[MinLengthValidator(10, '너무 짧군요! 10자 이상 적어주세요.')])
+    content = models.TextField(validators=[MinLengthValidator(10, '너무 짧군요! 10자 이상 적어주세요.'), validate_symbols])
     dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
 
