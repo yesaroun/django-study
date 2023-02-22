197938fb3d19b4ff4710132b79041cc8b4311be7
yesaroun <akor1@naver.com>
2023-02-22 15:27:12 +0900

    필드 인자로 유효성 검증하기

--- codeit-django/costory/posts/models.py
+++ codeit-django/costory/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     # 글의 제목, 내용, 작성일, 마지막 수정일
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True, error_messages={'unique': '이미 있는 제목이네요!'})
     content = models.TextField()
     dt_created = models.DateTimeField(verbose_name="Date Created", auto_now_add=True)
     dt_modified = models.DateTimeField(verbose_name="Date Modified", auto_now=True)
--- codeit-django/costory/posts/views.py
+++ codeit-django/costory/posts/views.py
@@ -18,9 +18,10 @@ def post_detail(request, post_id):
 def post_create(request):
     if request.method == 'POST':
         post_form = PostForm(request.POST)
-        new_post = post_form.save()
-
-        return redirect('post-detail', post_id=new_post.id)
+        if post_form.is_valid():
+            new_post = post_form.save()
+            return redirect('post-detail', post_id=new_post.id)
     else:
         post_form = PostForm()
-        return render(request, 'posts/post_form.html', {'form': post_form})
+    return render(request, 'posts/post_form.html', {'form': post_form})
+
