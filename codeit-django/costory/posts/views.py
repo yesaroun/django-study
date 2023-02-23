c332f5c08f7013d849d9ce25287421b783a93d36
yesaroun <akor1@naver.com>
2023-02-24 00:10:00 +0900

    feat: 수정하기 기능

--- codeit-django/costory/posts/views.py
+++ codeit-django/costory/posts/views.py
@@ -24,3 +24,15 @@ def post_create(request):
     else:
         post_form = PostForm()
     return render(request, "posts/post_form.html", {"form": post_form})
+
+
+def post_update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == "POST":
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect("post-detail", post_id=post_id)
+    else:
+        post_form = PostForm(instance=post)
+    return render(request, "posts/post_form.html", {"form": post_form})
