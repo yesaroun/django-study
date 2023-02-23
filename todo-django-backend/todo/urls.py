c51f0c82e1f63d19717fcf190d083af62eb95512
yesaroun <akor1@naver.com>
2023-02-23 16:36:04 +0900

    todo 작성하기

--- todo-django-backend/todo/urls.py
+++ todo-django-backend/todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.todo_list, name="todo_list"),
+    path("<int:pk>/", views.todo_detail, name="todo_detail"),
+    path("post/", views.todo_post, name="todo_post"),
 ]
--- todo-django-backend/todo/views.py
+++ todo-django-backend/todo/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 
 def todo_list(request):
     todos = Todo.objects.filter(complete=False)
     return render(request, "todo/todo_list.html", {"todos": todos})
+
+
+def todo_detail(request, pk):
+    todo = Todo.objects.get(id=pk)
+    return render(request, "todo/todo_detail.html", {"todo": todo})
+
+
+def todo_post(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.save()
+            return redirect("todo_list")
+        else:
+            form = TodoForm()
+        return render(request, "todo/todo_post.html", {"form": form})
+
+
+def todo_edit(request, pk):
+    todo = Todo.objects.get(id=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
