1c3a8f7fceeeb4d2b2d422aa13a4d89c80a8025d
yesaroun <akor1@naver.com>
2023-02-27 00:28:46 +0900

    fix: costory CreateView 수정

--- codeit-django/costory/posts/views.py
+++ codeit-django/costory/posts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.views import View
+
+# from django.views import View
+from django.views.generic import CreateView
+from django.urls import reverse
 from .models import Post
 from .forms import PostForm
 
@@ -31,16 +34,25 @@ def post_detail(request, post_id):
 #     else:
 #         post_form = PostForm()
 #     return render(request, "posts/post_form.html", {"form": post_form})
-class PostCreateView(View):
-    def get(self, request):
-        post_form = PostForm()
-        return render(request, "posts/post_form.html", {"form": post_form})
+# 클래스형 view
+# class PostCreateView(View):
+#     def get(self, request):
+#         post_form = PostForm()
+#         return render(request, "posts/post_form.html", {"form": post_form})
+#
+#     def post(self, request):
+#         post_form = PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save()
+#             return redirect("post-detail", post_id=new_post.id)
+# 클래스형 뷰, 제네릭 뷰(create)
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "posts/post_form.html"
 
-    def post(self, request):
-        post_form = PostForm(request.POST)
-        if post_form.is_valid():
-            new_post = post_form.save()
-            return redirect("post-detail", post_id=new_post.id)
+    def get_success_url(self):
+        return reverse("post-detail", kwargs={"post_id": self.object.id})
 
 
 def post_update(request, post_id):
