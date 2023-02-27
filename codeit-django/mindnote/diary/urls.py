92138f73c397030df2c3422bc751245c08adf245
yesaroun <akor1@naver.com>
2023-02-28 00:08:51 +0900

    fix: mindnote update view 적용

--- codeit-django/mindnote/diary/urls.py
+++ codeit-django/mindnote/diary/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path(
         "diary/page/<int:page_id>", views.PageDetailView.as_view(), name="page-detail"
     ),
-    path("diary/page/<int:page_id>/edit", views.page_update, name="page-update"),
+    path(
+        "diary/page/<int:page_id>/edit",
+        views.PageUpdateView.as_view(),
+        name="page-update",
+    ),
     path("diary/page/<int:page_id>/delete", views.page_delete, name="page-delete"),
 ]
