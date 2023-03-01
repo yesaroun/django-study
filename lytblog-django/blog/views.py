8ceedf9d6fa04edb894a3566f5143e46d1f1ef57
yesaroun <akor1@naver.com>
2023-03-01 20:32:04 +0900

    feat: 게시글 페이지 생성

--- lytblog-django/blog/views.py
+++ lytblog-django/blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.dates import (
+    ArchiveIndexView,
+    YearArchiveView,
+    MonthArchiveView,
+)
+from django.views.generic.dates import DayArchiveView, TodayArchiveView
+from blog.models import Post
 
-# Create your views here.
+
+# --- ListView
+class PostLV(ListView):
+    model = Post
+    template_name = "blog/post_all.html"
+    context_object_name = "posts"
+    paginate_by = 2
+
+
+# --- DetailView
+class PostDV(DetailView):
+    model = Post
+
+
+# --- Archiveview
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = "modify_dt"
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = "modify_dt"
+    make_object_list = True
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = "modify_dt"
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = "modify_dt"
+
+
+class PostDAV(TodayArchiveView):
+    model = Post
+    date_field = "modify_dt"
