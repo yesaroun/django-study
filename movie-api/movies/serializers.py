77d7eaebb1823521f80c97145133396d65a2d76c
yesaroun <akor1@naver.com>
2023-03-05 15:28:58 +0900

    fix: movie-api actor serializer을 modelserializer 클래스로 수정

--- movie-api/movies/serializers.py
+++ movie-api/movies/serializers.py
@@ -8,18 +8,7 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = ["id", "name", "opening_date", "running_time", "overview"]
 
 
-class ActorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    gender = serializers.CharField()
-    birth_date = serializers.DateField()
-
-    def create(self, validated_data):
-        return Actor.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.gender = validated_data.get("gender", instance.gender)
-        instance.birth_date = validated_data.get("birth_date", instance.birth_date)
-        instance.save()
-        return instance
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ["id", "name", "gender", "birth_date"]
