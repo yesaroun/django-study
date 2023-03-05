dff5af47d4b4ec08f3145f811bc60346eaf4b3d0
yesaroun <akor1@naver.com>
2023-03-05 15:00:18 +0900

    fix: ModelSerializer로 변경

--- movie-api/movies/serializers.py
+++ movie-api/movies/serializers.py
@@ -2,27 +2,10 @@ from rest_framework import serializers
 from .models import Movie, Actor
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    opening_date = serializers.DateField()
-    running_time = serializers.IntegerField()
-    overview = serializers.CharField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.opening_date = validated_data.get(
-            "opening_date", instance.opening_date
-        )
-        instance.running_time = validated_data.get(
-            "running_time", instance.running_time
-        )
-        instance.overview = validated_data.get("overview", instance.overview)
-        instance.save()
-        return instance
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ["id", "name", "opening_date", "running_time", "overview"]
 
 
 class ActorSerializer(serializers.Serializer):
