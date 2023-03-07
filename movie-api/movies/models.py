75614dd6defc9f9f2989fb87a208f4ab1ae23ca9
yesaroun <akor1@naver.com>
2023-03-07 13:58:51 +0900

    fix: movie-api actor 직렬화

--- movie-api/movies/models.py
+++ movie-api/movies/models.py
@@ -16,6 +16,9 @@ class Actor(models.Model):
     gender = models.CharField(max_length=1)
     birth_date = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
--- movie-api/movies/serializers.py
+++ movie-api/movies/serializers.py
@@ -3,12 +3,15 @@ from .models import Movie, Actor, Review
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    actors = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Movie
         fields = [
             "id",
             "name",
             "reviews",
+            "actors",
             "opening_date",
             "running_time",
             "overview",
@@ -26,6 +29,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
 
 class ActorSerializer(serializers.ModelSerializer):
+    movies = MovieSerializer(many=True, read_only=True)
+
     class Meta:
         model = Actor
-        fields = ["id", "name", "gender", "birth_date"]
+        fields = ["id", "name", "gender", "birth_date", "movies"]
