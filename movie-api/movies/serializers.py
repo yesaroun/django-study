123a707eb260dea192ab0d3f51e21aea973a9ee4
yesaroun <akor1@naver.com>
2023-03-07 11:33:58 +0900

    fix: movie-api nested serializer

--- movie-api/movies/serializers.py
+++ movie-api/movies/serializers.py
@@ -2,8 +2,18 @@ from rest_framework import serializers
 from .models import Movie, Actor, Review
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    movie = serializers.StringRelatedField()
+
+    class Meta:
+        model = Review
+        fields = ["id", "movie", "username", "star", "comment", "created"]
+
+
 class MovieSerializer(serializers.ModelSerializer):
-    reviews = serializers.StringRelatedField(many=True)
+    # Nested Serializer를 사용하려면
+    # MoviewSerializer 선언 전에 ReviewSerializer가 선언되어야 함.
+    reviews = ReviewSerializer(many=True, read_only=True)
 
     class Meta:
         model = Movie
@@ -21,11 +31,3 @@ class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = ["id", "name", "gender", "birth_date"]
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    movie = serializers.StringRelatedField()
-
-    class Meta:
-        model = Review
-        fields = ["id", "movie", "username", "star", "comment", "created"]
