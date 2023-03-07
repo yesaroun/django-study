1145e05dfa2740ec5ca1b76b6c67e2a3f060fbe9
yesaroun <akor1@naver.com>
2023-03-07 11:36:50 +0900

    fix: review serializer에 movie 정부 추가

--- movie-api/movies/serializers.py
+++ movie-api/movies/serializers.py
@@ -2,19 +2,7 @@ from rest_framework import serializers
 from .models import Movie, Actor, Review
 
 
-class ReviewSerializer(serializers.ModelSerializer):
-    movie = serializers.StringRelatedField()
-
-    class Meta:
-        model = Review
-        fields = ["id", "movie", "username", "star", "comment", "created"]
-
-
 class MovieSerializer(serializers.ModelSerializer):
-    # Nested Serializer를 사용하려면
-    # MoviewSerializer 선언 전에 ReviewSerializer가 선언되어야 함.
-    reviews = ReviewSerializer(many=True, read_only=True)
-
     class Meta:
         model = Movie
         fields = [
@@ -25,6 +13,16 @@ class MovieSerializer(serializers.ModelSerializer):
             "running_time",
             "overview",
         ]
+
+    read_only_fields = ["reviews"]
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    movie = MovieSerializer(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = ["id", "movie", "username", "star", "comment", "created"]
 
 
 class ActorSerializer(serializers.ModelSerializer):
