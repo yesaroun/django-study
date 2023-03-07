d6e491d7de351ca573a2a2ede95244f3bd64ba7d
yesaroun <akor1@naver.com>
2023-03-07 15:28:52 +0900

    fix: movie-api 클래스형 뷰: 데이터조회, 수정, 삭제

--- movie-api/movies/views.py
+++ movie-api/movies/views.py
@@ -35,19 +35,41 @@ class MovieList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET", "PATCH", "DELETE"])
-def movie_detail(request, pk):
-    movie = get_object_or_404(Movie, pk=pk)
-    if request.method == "GET":
+# @api_view(["GET", "PATCH", "DELETE"])
+# def movie_detail(request, pk):
+#     movie = get_object_or_404(Movie, pk=pk)
+#     if request.method == "GET":
+#         serializer = MovieSerializer(movie)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
+#     if request.method == "PATCH":
+#         serializer = MovieSerializer(movie, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == "DELETE":
+#         movie.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+class MovieDetail(APIView):
+    def get_object(self, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        return movie
+
+    def get(self, request, pk):
+        movie = self.get_object(pk)
         serializer = MovieSerializer(movie)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    if request.method == "PATCH":
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        movie = self.get_object(pk)
         serializer = MovieSerializer(movie, data=request.data, partial=True)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == "DELETE":
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        movie = self.get_object(pk)
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
