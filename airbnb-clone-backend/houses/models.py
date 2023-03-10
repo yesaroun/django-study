41a3ddd4414e4b72a9e77fee45eb36f2cae1f6be
yesaroun <akor1@naver.com>
2023-03-10 15:55:07 +0900

    fix: airbnb-clone fix admin

--- airbnb-clone-backend/houses/models.py
+++ airbnb-clone-backend/houses/models.py
@@ -6,10 +6,16 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name: str = models.CharField(max_length=140)
-    price_per_night: int = models.PositiveIntegerField()
+    price_per_night: int = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Numbers Only"
+    )
     description: str = models.TextField()
     address: str = models.CharField(max_length=140)
-    pets_allowed: bool = models.BooleanField(default=True)
+    pets_allowed: bool = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True,
+        help_text="Does this house allow pets?",
+    )
 
     def __str__(self) -> str:
         return self.name
