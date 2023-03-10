5ad399a5d306bfcd046095c05f4a71986e8fe220
yesaroun <akor1@naver.com>
2023-03-10 18:40:13 +0900

    feat: airbnb-clone common 모델 세팅

--- airbnb-clone-backend/common/models.py
+++ airbnb-clone-backend/common/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class CommonModel(models.Model):
+
+    """Common Model Definition"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # db에 저장 안하는 옵션
+        # 추상화
--- airbnb-clone-backend/rooms/models.py
+++ airbnb-clone-backend/rooms/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from common.models import CommonModel
 
 
-class Room(models.Model):
+class Room(CommonModel):
 
     """room Model Definition"""
 
@@ -39,11 +40,9 @@ class Room(models.Model):
     amenities = models.ManyToManyField(
         "rooms.Amenity",
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
 
-class Amenity(models.Model):
+class Amenity(CommonModel):
 
     """Amenity Definition"""
 
