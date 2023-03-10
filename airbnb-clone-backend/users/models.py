bb7415bc092e84bfdf115deb5759386b33599871
yesaroun <akor1@naver.com>
2023-03-10 18:01:59 +0900

    feat: airbnb-clone user 모델 생성

--- airbnb-clone-backend/users/models.py
+++ airbnb-clone-backend/users/models.py
@@ -3,7 +3,45 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, editable=False)
-    last_name = models.CharField(max_length=150, editable=False)
-    name = models.CharField(max_length=150, default="")
-    is_host = models.BooleanField(default=False)
+    class GenderChoices(models.TextChoices):
+        MALE: tuple = ("male", "Male")  # (db값, admin패널에서 보이는 label)
+        FEMALE: tuple = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR: tuple = ("kr", "Korean")
+        EN: tuple = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON: tuple = "won", "Korean Won"
+        USD: tuple = "usd", "Dollar"
+
+    first_name: str = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    last_name: str = models.CharField(
+        max_length=150,
+        editable=False,
+    )
+    avatar = models.ImageField(
+        blank=True,
+    )
+    name: str = models.CharField(
+        max_length=150,
+        default="",
+    )
+    is_host: bool = models.BooleanField(
+        default=False,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        max_length=2,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        max_length=5,
+        choices=CurrencyChoices.choices,
+    )
