5881aa63b63954703f78dc3c2a09f3c7035c7b93
yesaroun <akor1@naver.com>
2023-03-13 01:01:03 +0900

    feat: 별점 평균 메서드 추가

--- airbnb-clone-backend/rooms/models.py
+++ airbnb-clone-backend/rooms/models.py
@@ -60,6 +60,16 @@ class Room(CommonModel):
     def total_amenities(self) -> int:
         return self.amenities.count()
 
+    def rating(self):
+        count = self.reviews.count()
+        if count == 0:
+            return "No Reviews"
+        else:
+            total_rating = 0
+            for review in self.reviews.all().values("rating"):
+                total_rating += review["rating"]
+            return round(total_rating / count, 2)
+
 
 class Amenity(CommonModel):
 
