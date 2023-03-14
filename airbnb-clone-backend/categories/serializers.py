7d284e8327d1a02362854e733841813dc9fe9f3b
yesaroun <akor1@naver.com>
2023-03-14 11:21:07 +0900

    feat: airbnb-clone APIView

--- airbnb-clone-backend/categories/serializers.py
+++ airbnb-clone-backend/categories/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
+from .models import Category
 
 
-class CategorySerializers(serializers.Serializer):
+class CategorySerializer(serializers.Serializer):
+
+    pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(
-        max_length=50,
         required=True,
+        max_length=50,
     )
+    kind = serializers.ChoiceField(
+        choices=Category.CategoryKindChoices.choices,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+        return instance
--- airbnb-clone-backend/categories/views.py
+++ airbnb-clone-backend/categories/views.py
@@ -1,27 +1,52 @@
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.status import HTTP_204_NO_CONTENT
 from .models import Category
-from .serializers import CategorySerializers
+from .serializers import CategorySerializer
 
 
-@api_view(["GET", "POST"])
-def categories(request):
-
-    if request.method == "GET":
+class Categories(APIView):
+    def get(self, request):
         all_categories = Category.objects.all()
-        serializer = CategorySerializers(all_categories, many=True)
+        serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = CategorySerializers(data=request.data)
+
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
-            return Response({"created": True})
+            new_category = serializer.save()
+            return Response(
+                CategorySerializer(new_category).data,
+            )
         else:
             return Response(serializer.errors)
-    # {"name": "from DRF"}
 
 
-@api_view()
-def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializers(category)
-    return Response(serializer.data)
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        serializer = CategorySerializer(
+            self.get_object(pk),
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
