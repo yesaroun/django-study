bd09d203058d43137bb805fdba1853bbee15c0d5
yesaroun <akor1@naver.com>
2023-03-14 14:02:38 +0900

    fix: type hinting 적용

--- airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-backend/rooms/views.py
@@ -1,13 +1,19 @@
+from django.db.models import QuerySet
+from rest_framework.serializers import ListSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Amenity
 from .serializers import AmenitySerializer
+from typing import List
 
 
 class Amenities(APIView):
-    def get(self, request):
-        all_amenities = Amenity.objects.all()
-        serializer = AmenitySerializer(all_amenities, many=True)
+    def get(self, request) -> Response:
+        print(type(self), ": self")
+        all_amenities: QuerySet[Amenity] = Amenity.objects.all()
+        serializer: List[AmenitySerializer] = AmenitySerializer(
+            all_amenities, many=True
+        )
         return Response(serializer.data)
 
     def post(self, request):
