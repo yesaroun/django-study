c0cf958ab12484c221658ff9b0a667d0cdf78d19
yesaroun <akor1@naver.com>
2023-03-21 18:48:27 +0900

    feat: airbnb-clone pagination

--- airbnb-clone-backend/rooms/views.py
+++ airbnb-clone-backend/rooms/views.py
@@ -14,6 +14,7 @@ from .models import Amenity, Room
 from categories.models import Category
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from typing import List
+from reviews.serializers import ReviewSerializer
 
 
 class Amenities(APIView):
@@ -142,3 +143,27 @@ class RoomDetail(APIView):
             raise PermissionDenied
         room.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class RoomReviews(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+        room = self.get_object(pk)
+        serializer = ReviewSerializer(
+            room.reviews.all()[start:end],
+            many=True,
+        )
+        return Response(serializer.data)
