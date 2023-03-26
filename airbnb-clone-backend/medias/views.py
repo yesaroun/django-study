fc1608f652405893246933757035fe0d8b9cbddb
yesaroun <akor1@naver.com>
2023-03-26 14:26:06 +0900

    upload photo

--- airbnb-clone-backend/medias/views.py
+++ airbnb-clone-backend/medias/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound, PermissionDenied
+from .models import Photo
 
-# Create your views here.
+
+class PhotoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Photo.objects.get(pk=pk)
+        except Photo.DoesNotExist:
+            raise NotFound
+
+    def delete(self, request, pk):
+        photo = self.get_object(pk)
+        if photo.room:
+            if photo.room.owner != request.user:
+                raise PermissionDenied
