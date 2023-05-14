63d711d37bf68d20d43bfc7a37efb7e37f78d4a3
yesaroun <akor1@naver.com>
2023-05-14 21:28:48 +0900

    feat: coplate - nickname 필드 추가하기

--- codeit-django/django-coplate/coplate_project/coplate/models.py
+++ codeit-django/django-coplate/coplate_project/coplate/models.py
@@ -3,5 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    nickname = models.CharField(
+        max_length=15,
+        unique=True,
+        null=True,
+    )
+
     def __str__(self) -> str:
         return self.email
