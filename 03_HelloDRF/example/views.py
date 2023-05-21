4cc3b81d8f0e299ed1a6c70eeab0035704029d28
yesaroun <akor1@naver.com>
2023-05-22 04:24:47 +0900

    feat: drf 책 api serializer 세팅

---
+++ 03_HelloDRF/example/views.py
@@ -0,0 +1,34 @@
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
+from .models import Book
+from .serializers import BookSerializer
+
+
+@api_view(['GET'])
+def HelloAPI(request):
+    return Response("hello world!")
+
+
+@api_view(['GET', 'POST'])
+def booksAPI(request):
+    if request.method == "GET":
+        books = Book.objects.all() 
+        serializer = BookSerializer(books, maney=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "POST":
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save() 
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def bookAPI(request, bid):
+    book = get_object_or_404(Book, bid=bid)
+    serializer = BookSerializer(book)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
--- 03_HelloDRF/myweb/settings.py
+++ 03_HelloDRF/myweb/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'example.apps.ExampleConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
