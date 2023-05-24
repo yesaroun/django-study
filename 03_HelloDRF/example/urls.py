03ac9125910c863ae9f77b9b8559f2d7f68b7721
yesaroun <akor1@naver.com>
2023-05-24 22:08:52 +0900

    feat: drf mixinx api 생성

--- 03_HelloDRF/example/urls.py
+++ 03_HelloDRF/example/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path, include
-from .views import BookAPI, BooksAPI, helloAPI, bookAPI, booksAPI
+from .views import (
+    BookAPI,
+    BooksAPI,
+    helloAPI,
+    bookAPI,
+    booksAPI,
+    BooksAPIMixins,
+    BookAPIMixins,
+)
 
 urlpatterns = [
     path("hello/", helloAPI),
     path("fbv/books/", booksAPI),
     path("fbv/book/<int:bid>/", bookAPI),
-    path("fbv/Books/", BooksAPI.as_view()),
-    path("fbv/Book/<int:bid>/", BookAPI.as_view()),
+    path("cbv/books/", BooksAPI.as_view()),
+    path("cbv/book/<int:bid>/", BookAPI.as_view()),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view()),
 ]
-
--- 03_HelloDRF/example/views.py
+++ 03_HelloDRF/example/views.py
@@ -66,11 +66,15 @@ class BooksAPIMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.Ge
         return self.create(request, *args, **kwargs)
 
 
-class BookAPIMixins(mixins.RetrieveModelMixin, generics.GenericAPIView):
+class BookAPIMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     lookup_field = 'bid' 
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
