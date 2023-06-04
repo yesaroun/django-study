20a9f0eaeb0638ad28df234467604a8be3fb77e6
yesaroun <akor1@naver.com>
2023-06-04 19:30:11 +0900

    feat: podomarket - 수정, 삭제 기능 완성

--- codeit-django/django-podomarket/podomarket_project/podomarket/views.py
+++ codeit-django/django-podomarket/podomarket_project/podomarket/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import ListView, DeleteView, CreateView
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
 from allauth.account.views import PasswordChangeView
 from .models import Post
 from .forms import PostForm
@@ -30,10 +30,29 @@ class PostCreateView(CreateView):
             "post-detail",
             kwargs={"post_id": self.object.id},
         )
-    
+
     def form_valid(self, form):
-        form.instance.author = self.request.user 
+        form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "podomarket/post_form.html"
+    pk_url_kwarg = "post_id"
+
+    def get_success_url(self):
+        return reverse("post-detail", kwargs={"post_id": self.object.id})
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "podomarket/post_confirm_delete.html"
+    pk_url_kwarg = "post_id"
+    
+    def get_success_url(self) -> str:
+        return reverse("index")
 
 
 class CustomPasswordChangeView(PasswordChangeView):
