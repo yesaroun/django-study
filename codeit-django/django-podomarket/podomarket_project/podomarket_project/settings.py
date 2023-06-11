c3314ec5cae5224a2a9dc7688a78f9eca0bb614c
yesaroun <akor1@naver.com>
2023-06-12 01:13:52 +0900

    feat: podomarket - middleware

--- codeit-django/django-podomarket/podomarket_project/podomarket_project/settings.py
+++ codeit-django/django-podomarket/podomarket_project/podomarket_project/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "podomarket.middleware.ProfileSetupMiddleware",
 ]
 
 ROOT_URLCONF = "podomarket_project.urls"
