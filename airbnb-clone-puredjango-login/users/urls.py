4ffba33e5ee5f139d495916a87f8bd85723a60ea
yesaroun <akor1@naver.com>
2023-06-15 05:03:03 +0900

    airbnb-pure [feat] login and logout

--- airbnb-clone-puredjango-login/users/urls.py
+++ airbnb-clone-puredjango-login/users/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "uesrs"
 
 urlpatterns = [
-    path("login", views.LoginView.as_view(), name="login")
+    path("login", views.LoginView.as_view(), name="login"),
+    path("logout", views.log_out, name="logout"),
 ]
