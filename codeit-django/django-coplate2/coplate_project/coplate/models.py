aa9f89bfe79d78d1edd7625b9203b15c48a8ceb6
yesaroun <akor1@naver.com>
2023-06-25 11:47:07 +0900

    [feat] coplate2 - 다대다 관계 구현

--- codeit-django/django-coplate2/coplate_project/coplate/models.py
+++ codeit-django/django-coplate2/coplate_project/coplate/models.py
@@ -21,6 +21,8 @@ class User(AbstractUser):
     )
 
     intro = models.CharField(max_length=60, blank=True)
+    
+    following = models.ManyToManyField("self", symmetrical=False)
 
     def __str__(self):
         return self.email
