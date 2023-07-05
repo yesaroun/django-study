2aa085e89a73f1bcb866329d57b510f662c0aa49
yesaroun <akor1@naver.com>
2023-07-05 13:37:12 +0900

    [feat] airbnb clone custom model

--- nomad/airbnb-clone-backend/config/settings.py
+++ nomad/airbnb-clone-backend/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 CUSTOM_APPS = [
     "houses.apps.HousesConfig",
+    "users.apps.UsersConfig",
 ]
 
 SYSTEM_APPS = [
@@ -128,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Auth 
+AUTH_USER_MODEL = "users.User"
