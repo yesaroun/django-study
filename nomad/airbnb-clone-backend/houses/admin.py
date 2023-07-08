54caffd373f64a68c99339646af513a61610771b
yesaroun <akor1@naver.com>
2023-07-08 17:15:46 +0900

    [feat] airbnb-clone User app - recap

--- nomad/airbnb-clone-backend/houses/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from houses.models import House
-
-
-@admin.register(House)
-class HouseAdmin(admin.ModelAdmin):
-    list_display = ("name", "price_per_night", "address", "pets_allowed")
-    list_filter = ("price_per_night", "pets_allowed")
-    search_fields = ("address",)
