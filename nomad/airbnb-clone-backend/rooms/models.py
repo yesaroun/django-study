32154c3c7fc6da25aff247e7241135235f07f6bc
yesaroun <akor1@naver.com>
2023-07-15 17:28:09 +0900

    [feat] airbnb-clone models and admin rooms admin

--- nomad/airbnb-clone-backend/rooms/models.py
+++ nomad/airbnb-clone-backend/rooms/models.py
@@ -10,6 +10,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = ("shared_room", "Shared Room")
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -22,9 +23,18 @@ class Room(CommonModel):
     owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
     amenities = models.ManyToManyField("rooms.Amenity")
 
+    def __str__(self):
+        return self.name
+
 
 class Amenity(CommonModel):
     """Amenity Definition"""
 
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
