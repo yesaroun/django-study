7d36de66cefa606c068b3231f0b1e0711dfbe597
yesaroun <akor1@naver.com>
2023-08-13 00:44:30 +0900

    [feat] airbnb clone 11.3

--- nomad/airbnb-clone-backend/experiences/urls.py
+++ nomad/airbnb-clone-backend/experiences/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from experiences.views import PerkDetail, Perks
 
 urlpatterns = [
-    path("perks/", Perk.as_view()),
+    path("perks/", Perks.as_view()),
     path("perks/<int:pk>", PerkDetail.as_view()),
 ]
