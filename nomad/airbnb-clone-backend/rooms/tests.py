961a08ecd3f0136b1ef6c767910ba5c6cda511e5
yesaroun <akor1@naver.com>
2023-08-22 21:59:59 +0900

    api testing

--- nomad/airbnb-clone-backend/rooms/tests.py
+++ nomad/airbnb-clone-backend/rooms/tests.py
@@ -6,6 +6,7 @@ from rooms import models
 class TestAmenities(APITestCase):
     NAME = "Amenity Test"
     DESC = "Amenity Des"
+    URL = "api/v1/rooms/amenities/"
 
     def setUp(self) -> None:
         models.Amenity.objects.create(
@@ -14,7 +15,7 @@ class TestAmenities(APITestCase):
         )
 
     def test_all_amenites(self):
-        response = self.client.get("/api/v1/rooms/amenities/")
+        response = self.client.get(self.URL)
         data = response.json()
 
         self.assertEqual(
@@ -38,3 +39,36 @@ class TestAmenities(APITestCase):
             data[0]["description"],
             self.DESC,
         )
+
+    def test_create_amenity(self):
+        new_amnity_name = "New Amenity"
+        new_amnity_description = "New Amenity desc."
+
+        response = self.client.post(
+            self.URL,
+            data={
+                "name": new_amnity_name,
+                "description": new_amnity_description,
+            },
+        )
+        data = response.json()
+
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
+        self.assertEqual(
+            data["name"],
+            new_amnity_name,
+        )
+        self.assertEqual(
+            data["description"],
+            new_amnity_description,
+        )
+
+        response = self.client.post(self.URL)
+        data = response.json()
+
+        self.assertEqual(response.status_code, 400)
+        self.assertIn("name", data)
--- nomad/airbnb-clone-backend/rooms/views.py
+++ nomad/airbnb-clone-backend/rooms/views.py
@@ -7,7 +7,10 @@ from rest_framework.exceptions import (
     ParseError,
 )
 from rest_framework.response import Response
-from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.status import (
+    HTTP_204_NO_CONTENT,
+    HTTP_400_BAD_REQUEST,
+)
 
 from drf_yasg.utils import swagger_auto_schema
 
@@ -42,7 +45,10 @@ class Amenities(APIView):
             amenity = serializer.save()
             return Response(AmenitySerializer(amenity).data)
         else:
-            return Response(serializer.errors)
+            return Response(
+                serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
 
 
 class AmenityDetail(APIView):
